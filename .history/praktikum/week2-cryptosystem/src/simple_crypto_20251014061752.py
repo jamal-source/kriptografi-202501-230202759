12743e8f64a98800b6f83080d04ec13f70531364
jamal-source <jud2272@gmail.com>
2025-10-14 18:06:11 +0700

    week2-cryptosystem

---
+++ .history/praktikum/week2-cryptosystem/src/simple_crypto_20251014061752.py
@@ -0,0 +1,13 @@
+#simple_crypto.py
+
+def encrypt(plaintext, key):
+    result = ""
+    for char in plaintext:
+        if char.isalpha():
+            shift = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift + key) % 26 + shift)
+        else:
+            result += char
+    return result
+
+
---
+++ .history/praktikum/week2-cryptosystem/src/simple_crypto_20251014061812.py
@@ -0,0 +1,26 @@
+#simple_crypto.py
+
+ss
+
+def decrypt(ciphertext, key):
+    result = ""
+    for char in ciphertext:
+        if char.isalpha():
+            shift = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift - key) % 26 + shift)
+        else:
+            result += char
+    return result
+
+
+if __name__ == "__main__":
+    message = "Cryptosystem Test"
+    key = 5
+
+    enc = encrypt(message, key)
+    dec = decrypt(enc, key)
+
+    print("Plaintext :", message)
+    print("Ciphertext:", enc)
+    print("Decrypted :", dec)
+
