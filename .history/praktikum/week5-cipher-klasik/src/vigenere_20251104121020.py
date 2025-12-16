4c9ea47a207dc40ab0e9ad8a2449761f99eebc8f
jamal-source <jud2272@gmail.com>
2025-12-16 17:41:37 +0700

    week5-cipher-klasik

---
+++ .history/praktikum/week5-cipher-klasik/src/vigenere_20251104121020.py
@@ -0,0 +1,37 @@
+def vigenere_encrypt(plaintext, key):
+    result = []
+    key = key.lower()
+    key_index = 0
+    for char in plaintext:
+        if char.isalpha():
+            shift = ord(key[key_index % len(key)]) - 97
+            base = 65 if char.isupper() else 97
+            result.append(chr((ord(char) - base + shift) % 26 + base))
+            key_index += 1
+        else:
+            result.append(char)
+    return "".join(result)
+
+def vigenere_decrypt(ciphertext, key):
+    result = []
+    key = key.lower()
+    key_index = 0
+    for char in ciphertext:
+        if char.isalpha():
+            shift = ord(key[key_index % len(key)]) - 97
+            base = 65 if char.isupper() else 97
+            result.append(chr((ord(char) - base - shift) % 26 + base))
+            key_index += 1
+        else:
+            result.append(char)
+    return "".join(result)
+
+# Contoh uji
+if __name__ == "__main__":
+    msg = "KRIPTOGRAFI"
+    key = "KEY"
+    enc = vigenere_encrypt(msg, key)
+    dec = vigenere_decrypt(enc, key)
+    print("Plaintext :", msg)
+    print("Ciphertext:", enc)
+    print("Decrypted :", dec)
